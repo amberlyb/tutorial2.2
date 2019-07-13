66dbd6e12aa0beeb615b9e1b29d5da5f1cc7f9f5
Amberly Brewster <amberlybrewster@Amberlys-iMac.local>
2019-07-13 00:29:31 -0400

    Challenge 2 Working

---
+++ Maze Game/Maze/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+public float min = 0f;
+public float max = 0f;
+
+    void Start()
+    {
+        min = transform.position.x;
+        max = transform.position.x+1;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time, max-min)+min, transform.position.y, transform.position.z);
+        transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
+    }
+}
--- Maze Game/Maze/Assets/Scripts/PlayerController.cs
+++ Maze Game/Maze/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,15 +9,37 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb2d;    
     public float speed;
     public float jumpForce;
+    public Text scoreText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
+
+    private int score;
+    private int lives;
 
     void Start()
     {
+
         rb2d = GetComponent<Rigidbody2D>();
+        score = 0;
+        lives = 3;
+        winText.text = "";
+        loseText.text = "";
+        SetAllText ();
     }
 
     void Update()
     {
-        
+       
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+
+        if (score == 8)
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
     }
 
     void FixedUpdate()
@@ -24,16 +47,48 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(moveHorizontal, 0);
         rb2d.AddForce(movement * speed);
-        if (Input.GetKey("escape"))
-            Application.Quit();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pick Ups"))
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            SetAllText ();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetAllText ();
+        }
+        if (score == 4) 
+            transform.position = new Vector2 (22.68f, -2.44f);
     }
 
     void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.collider.tag == "Ground") {
-            if(Input.GetKey(KeyCode.UpArrow)) {
+        if(collision.collider.tag == "Ground") 
+        {
+            if(Input.GetKey(KeyCode.UpArrow)) 
+            {
                 rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
+        }
+    }
+
+    void SetAllText()
+    {
+        scoreText.text = "Score: " + score.ToString ();
+        if (score >= 8)
+            winText.text = "You win!";
+        livesText.text = "Lives: " + lives.ToString ();
+        if (lives <= 0)
+        {
+            loseText.text = "You lose!";
+            Destroy(this);
         }
     }
 }
